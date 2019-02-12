84dae92e7496178b163e97dec924e4dc859d278c
unknown <marcosnobre26@gmail.com>
2019-02-12 09:56:30 -0300

    odificações no sistema de login e inscrição

--- login/perfil.php
+++ login/perfil.php
@@ -5,18 +5,38 @@
     $id=$_POST['id'];
     $nome=$_POST['nome'];
     $foto=$_POST['foto'];
+    $tipo=$_POST['tipo'];
+    $senha=$_POST['senha'];
     
     $pdo = new PDO("mysql:host=localhost;dbname=simulador", "root", "");    
      
     try {
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);        
-        $stmt = $pdo->prepare("INSERT INTO tb_login_facebook (nome, foto, id_facebook) VALUES('$nome', '$id', '$foto')");      
-        //echo $stmt->rowCount();
+        $cont=0;
+        $consulta = $pdo->query("SELECT nome, id_conta FROM perfil;");
+        while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
+            //echo "Nome: {$linha['nome']} - Usuário: {$linha['id_conta']}<br />";
+
+            if($linha['nome']===$nome && $linha['id_conta']===$id)
+            {
+                $cont=$cont+1;//quer dizer que o registro já existe
+            }
+        }
+
+        if($cont==0){
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);        
+            $stmt = $pdo->prepare("INSERT INTO perfil (nome, foto, id_conta, tipo, senha) VALUES(:nome, :foto, :id_conta, :tipo, :senha)");      
+            $stmt->execute(array(
+                ':nome' => $nome,
+                ':foto' => $foto,
+                ':id_conta' => $id,
+                ':tipo' => $tipo,
+                ':senha' => $senha
+              ));
+            
+            echo $stmt->rowCount();
+        }
+        
     } catch(PDOException $e) {
         echo 'Error: ' . $e->getMessage();
     }
-
-
-
-
 ?>
